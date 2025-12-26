49511278153a833d9b9903fdb802714088aa1a7d
Kanami Anderson <kanamianderson@gmail.com>
2025-12-26 15:01:59 -0600

    Practiced Optional Values and Nullish Coalescing in TypeScript course.

---
+++ optional.js
@@ -0,0 +1,8 @@
+// function generateError(msg?: string) {
+//     throw new Error(msg) 
+// }
+var user = {
+    name: 'Kanami',
+    age: 42,
+};
+console.log(user);
--- optional.ts
+++ optional.ts
@@ -1,3 +1,22 @@
-function generateError(msg: string) {
-    throw new Error(msg) 
+// function generateError(msg?: string) {
+//     throw new Error(msg) 
+// }
+
+// generateError();
+
+type User = {
+    name: string;
+    age: number;
+    role?: 'admin' | 'guest'
+};
+
+const user: User = {
+    name: 'Kanami',
+    age: 42,
 }
+
+console.log(user);
+
+let input = null;
+
+const didProvideInput = input || false
