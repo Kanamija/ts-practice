39f7c680795d4dffe15e4992ceeb0dda0f6b76ec
Kanami Anderson <kanamianderson@gmail.com>
2026-01-07 12:05:38 -0600

    Practiced using Generic Types in Objects and Functions.

---
+++ generics.js
@@ -0,0 +1,9 @@
+var names = ['Max', 'Anna'];
+var store = {};
+store.name = 'Max';
+store.isInstructor = true;
+var nameStore = {};
+function merge(a, b) {
+    return [a, b];
+}
+var ids = merge(1, 2);
---
+++ generics.ts
@@ -0,0 +1,17 @@
+let names: Array<string> = ['Max', 'Anna']
+
+type DataStore<T> = {
+    [key: string]: T
+};
+
+let store: DataStore<string | boolean> = {};
+store.name = 'Max';
+store.isInstructor = true;
+
+let nameStore: DataStore<string> = {};
+
+function merge<T>(a: T, b: T) {
+    return [a, b];
+}
+
+const ids = merge(1, 2);
